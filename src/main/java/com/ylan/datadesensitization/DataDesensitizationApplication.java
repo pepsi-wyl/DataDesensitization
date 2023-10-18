ae039574537231b7981665c43873351e2eb3fbd3
pepsi-wyl <2322535585@qq.com>
2023-10-18 20:44:25 +0800

    初始化工程

---
+++ src/main/java/com/ylan/datadesensitization/DataDesensitizationApplication.java
@@ -0,0 +1,13 @@
+package com.ylan.datadesensitization;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataDesensitizationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DataDesensitizationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ylan/datadesensitization/DataDesensitizationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ylan.datadesensitization;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataDesensitizationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
